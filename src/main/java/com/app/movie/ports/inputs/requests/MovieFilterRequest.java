d9939f2e79c20cd5e260bb2be97a65ce045db6a4
Cristian Aguirre <cristianaaguirre0@gmail.com>
2022-05-29 09:48:11 -0300

    add sorted property

--- src/main/java/com/app/movie/ports/inputs/requests/MovieFilterRequest.java
+++ src/main/java/com/app/movie/ports/inputs/requests/MovieFilterRequest.java
@@ -3,10 +3,12 @@ package com.app.movie.ports.inputs.requests;
 import lombok.*;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor @AllArgsConstructor
 public class MovieFilterRequest {
    @NotEmpty private String name;
    @NotEmpty private String genre;
+   @NotEmpty @Size(max = 4) private String sort;
 }
